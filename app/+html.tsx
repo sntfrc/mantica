f14253796158368e6c7502ff167361666864f4a3
Federico Santandrea <federico@santandrea.net>
2024-10-25 16:56:19 +0200

    Fixed meta headers case

--- app/+html.tsx
+++ app/+html.tsx
@@ -5,8 +5,8 @@ export default function Root({ children }: PropsWithChildren) {
   return (
     <html lang="en">
       <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
         <link rel="manifest" href="manifest.json" />
